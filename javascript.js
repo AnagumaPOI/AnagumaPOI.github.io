e1c1817dd9c1fda7205f5d9ec8e3a788edaa74c4
AnagumaPOI <75058634+AnagumaPOI@users.noreply.github.com>
2020-11-26 13:01:52 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function show_close(id){
+    var e = document.getElementById(id) ;
+    if (e.style.display == 'none')
+        e.style.display = 'block';
+    else
+        e.style.display = 'none';
+};
+
